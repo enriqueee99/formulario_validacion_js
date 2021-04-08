a0b550a97fb69d0b966df3b9724b423026cff977
Enrique <enriqueee99@gmail.com>
2021-04-08 12:04:37 -0500

    reutiza la validacion anterior de un campo en una funcion

--- js/formulario.js
+++ js/formulario.js
@@ -11,23 +11,44 @@ const expresiones = {
 
 const validarFormulario = (e) =>{
     switch(e.target.name){
-        case "usuario": 
-        if(expresiones.usuario.test(e.target.value)){
-           document.getElementById('formulario__usuario').classList.remove('formulario__grupo-incorrecto');
-           document.getElementById('formulario__usuario').classList.add('formulario__grupo-correcto');
-           document.querySelector('#formulario__usuario i').classList.add('fa-check-circle');
-           document.querySelector('#formulario__usuario i').classList.remove('fa-times-circle');
-           document.querySelector('#formulario__usuario .formulario__input-error').classList.remove('formulario__input-error-activo');
-        } else {
-            document.getElementById('formulario__usuario').classList.add('formulario__grupo-incorrecto');
-            document.getElementById('formulario__usuario').classList.remove('formulario__grupo-correcto');
-            document.querySelector('#formulario__usuario i').classList.add('fa-times-circle');
-            document.querySelector('#formulario__usuario i').classList.remove('fa-check-circle');
-            document.querySelector('#formulario__usuario .formulario__input-error').classList.add('formulario__input-error-activo');
-        } 
+        case 'usuario': 
+            validarCampos(expresiones.usuario, e.target, 'usuario');
         break;
+        case 'nombre':
+            validarCampos(expresiones.nombre, e.target, 'nombre');
     };
 };
+
+const validarCampos = (expresion, input, campo) =>{
+    if(expresion.test(input.value)){
+        document.getElementById(`formulario__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`formulario__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#formulario__${campo} i`).classList.add('fa-check-circle');
+        document.querySelector(`#formulario__${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#formulario__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+     } else {
+         document.getElementById(`formulario__${campo}`).classList.add('formulario__grupo-incorrecto');
+         document.getElementById(`formulario__${campo}`).classList.remove('formulario__grupo-correcto');
+         document.querySelector(`#formulario__${campo} i`).classList.add('fa-times-circle');
+         document.querySelector(`#formulario__${campo} i`).classList.remove('fa-check-circle');
+         document.querySelector(`#formulario__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+     } 
+}
+
+
+/* expresion = expresiones.usuario
+input = e.target.value
+campo = usuario */
+
+/* const validarCampos = () =>{
+    if(expresiones.usuario.test(e.target.value)){
+        document.getElementById('formulario__usuario').classList.remove('formulario__grupo-incorrecto');
+        document.getElementById('formulario__usuario').classList.add('formulario__grupo-correcto');
+     } else {
+         document.getElementById('formulario__usuario').classList.add('formulario__grupo-incorrecto');
+         document.getElementById('formulario__usuario').classList.remove('formulario__grupo-correcto');
+     } 
+} */
 
 
 inputs.forEach((input) =>{
